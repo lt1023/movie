93c469ee9ec59232fa805d22703110a9505d6d09
zero <1023968391@qq.com>
2019-02-13 15:50:44 +0800

    push

--- app/src/main/java/com/yellow/SplashActivity.java
+++ app/src/main/java/com/yellow/SplashActivity.java
@@ -1,8 +1,13 @@
 package com.yellow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+
+import com.yellow.login.view.LoginActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,7 +15,23 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        //Toast.makeText(this , "hello" , Toast.LENGTH_SHORT).show();
+        checkLogin();
+    }
+
+    private void checkLogin() {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                // task to run goes here
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                SplashActivity.this.finish();
+            }
+        };
+        Timer timer = new Timer();
+        long delay = 3000;
+        long intevalPeriod = 1 * 1000;
+        // schedules the task to be run in an interval
+        timer.scheduleAtFixedRate(task, delay, intevalPeriod);
     }
 
 }
