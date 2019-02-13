f2126d5b77d7f774992e82594842055a899953df
zero <1023968391@qq.com>
2019-02-13 15:11:27 +0800

    splash

--- app/src/main/java/com/yellow/SplashActivity.java
+++ app/src/main/java/com/yellow/SplashActivity.java
@@ -2,13 +2,15 @@ package com.yellow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash);
+        //Toast.makeText(this , "hello" , Toast.LENGTH_SHORT).show();
     }
 
 }
